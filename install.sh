bbc57bad360121026583c6f9049ef244300f4ade
Alex Southgate <southgate@gmail.com>
2013-05-27 12:38:01 -0700

    Add some needed directories, davfs, and my public key

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ sudo apt-get update
 sudo apt-get install puppet-common git
 sudo mv /etc/puppet /etc/puppet.bak
 sudo git clone https://github.com/fromagie/puppet.git /etc/puppet
+sudo chown -R pi:pi /etc/puppet
--- manifests/config.pp
+++ manifests/config.pp
@@ -13,24 +13,55 @@ package { "htop":
     ensure => "latest"
 }
 
-file { '/home/pi/.bashrc':
+package { "rtorrent":
+    ensure => "latest"
+}
+
+package { "screen":
+    ensure => "latest"
+}
+
+package { "davfs2":
+    ensure => "latest"
+}
+
+file { "/home/pi/.ssh":
+  ensure => 'directory',
+  owner  => 'pi',
+  group  => 'pi',
+} 
+
+# rtorrent session
+file { "/home/pi/.session":
+  ensure => 'directory',
+  owner  => 'pi',
+  group  => 'pi',
+} 
+
+file { "/media/bt":
+  ensure => 'directory',
+  owner  => 'pi',
+  group  => 'pi',
+}
+
+file { "/media/downloads":
+  ensure => 'directory',
+  owner  => 'pi',
+  group  => 'pi',
+}
+
+# See http://reductivelabs.com/trac/puppet/wiki/TypeReference#ssh-authorized-key
+ssh_authorized_key { "alex-rsa-key":
+   ensure => 'present',
+   key => 'AAAAB3NzaC1yc2EAAAABIwAAAIEA4Qxi6tH2b0rmvdKcRSUu+wyMtCJelxmVJ6/EMZAlwgeBVPdMJTlTQSPlt7I6XQ2IrKtOjBSOiYLjlbT0HkMvo1oqEaW5WDLZVCbqVAtxMPs3ebKUHFiNJ5RY8UZby74k0rCbMEZaMAe/eBAN07j9zNwITaOdNXyG30yZmMzsxOE=',
+   type => 'rsa',
+   user => 'pi',
+   require => File["/home/pi/.ssh"],
+}
+
+file { '/home/pi/.rtorrent.rc':
     owner => 'pi',
     group => 'pi',
     mode => 0644,
-    source => "puppet:///modules/environment/.bashrc",
+    source => "puppet:///modules/rtorrent/rtorrent.rc",
 }
-
-file { '/home/pi/.vimrc':
-    owner => 'pi',
-    group => 'pi',
-    mode => 0644,
-    source => "puppet:///modules/environment/.vimrc",
-}
-
-file { '/home/pi/.gitconfig':
-    owner => 'pi',
-    group => 'pi',
-    mode => 0644,
-    source => "puppet:///modules/environment/.gitconfig",
-}
-
